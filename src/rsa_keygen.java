cfd976c679cb9f5f7ac53766b74452c5e698113f
js395 <js395@st-andrews.ac.uk>
2019-10-18 22:47:19 +0100

    improved commenting + incorrect input handling

--- src/rsa_keygen.java
+++ src/rsa_keygen.java
@@ -10,6 +10,7 @@ public class rsa_keygen {
 //        rsa_keygen x =new rsa_keygen();
         BigInteger test_val_1 = new BigInteger("15");
         BigInteger[] values = readInValues(args[0]);
+
         System.out.println(isPrime(new BigInteger("15"), new BigInteger("3"), new BigInteger("5")));
     }
 
@@ -18,7 +19,7 @@ public class rsa_keygen {
      * @param x operand 1
      * @param p operand 2
      * @param q operand 3
-     * @return
+     * @return true if e and (p-1)(q-1) are coprime and false if not
      */
     public static boolean isPrime(BigInteger x, BigInteger p, BigInteger q) {
         BigInteger operand_2 = (p.subtract(new BigInteger("1"))).multiply(q.subtract(new BigInteger("1")));
@@ -30,8 +31,8 @@ public class rsa_keygen {
 
     /**
      * applies Euclid's algorithm recursively to determine the greatest common divisor between the parameters a and b
-     * @param a
-     * @param b
+     * @param a operand 1
+     * @param b (p-1)(q-1)
      * @return The greatest common divisor between a and b
      */
     private static BigInteger gcd(BigInteger a, BigInteger b) {
@@ -41,6 +42,11 @@ public class rsa_keygen {
         return gcd(b.mod(a), a);
     }
 
+    /**
+     * This reads in a file containing 3 numbers, into an array of 3 BigIntegers
+     * @param infile The input file containing
+     * @return
+     */
     private static BigInteger[] readInValues(String infile) {
         try {
             String[] numbers = new String[3];
@@ -51,10 +57,10 @@ public class rsa_keygen {
                 numbers[i] = scanner.next();
                 values[i] = new BigInteger(numbers[i]);
             }
-
             return values;
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("Unsuitable exponent\n");
+            System.exit(-1);
         }
         return null;
     }
